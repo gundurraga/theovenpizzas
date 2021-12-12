ea40e2659075427d60511d8323e4fb5d4aa51a3b
gundurraga <67235552+gundurraga@users.noreply.github.com>
2021-12-11 22:17:43 -0300

    compress images

--- js/index.js
+++ js/index.js
@@ -1,2 +1,4 @@
 console.log("Welcome fellow developer 🍕 happy hacking 🍕");
-console.log("You can visit the repository here: ");
+console.log(
+  "You can visit the repository here: https://github.com/gundurraga/theovenpizzas"
+);
